bbe3392a58df2366de598642f685645394c22319
leifeng <289528400@qq.com>
2013-03-04 17:59:45 +0800

    用户管理设计

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -2,11 +2,19 @@ var redis=require('../RedisHelp');
 var index=function(req,res){
     if(!req.session.admin&&!req.session.length)
     {
-        res.render('admin/login.html',{info:'请先登陆'});
+        res.render('admin/login.html',{info:'请先登陆',query:""});
     }
     else{
+
         redis.allUser(function(Users){
-            res.render('admin/index.html',{admin:req.session.admin,Users:Users});
+            if(req.query.id!=null){
+                var uid=req.query.id;
+                redis.stopUser(uid);
+                res.render('admin/index.html',{admin:req.session.admin,Users:Users,query:req.query.id});
+            }else{
+                res.render('admin/index.html',{admin:req.session.admin,Users:Users,query:""});
+            }
+
         });
 
     }
