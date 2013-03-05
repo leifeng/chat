6dc361ea0d3b5804fc7fe289e7f4b14bd46797f3
leifeng <289528400@qq.com>
2013-03-05 14:04:33 +0800

    删除和状态

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -1,22 +1,29 @@
-var redis=require('../RedisHelp');
-var index=function(req,res){
-    if(!req.session.admin&&!req.session.length)
-    {
-        res.render('admin/login.html',{info:'请先登陆',query:""});
+var redis = require('../RedisHelp');
+var index = function (req, res) {
+    if (!req.session.admin && !req.session.length) {
+        res.render('admin/login.html', {info:'请先登陆'});
     }
-    else{
-
-        redis.allUser(function(Users){
-            if(req.query.id!=null){
-                var uid=req.query.id;
-                redis.stopUser(uid);
-                res.render('admin/index.html',{admin:req.session.admin,Users:Users,query:req.query.id});
-            }else{
-                res.render('admin/index.html',{admin:req.session.admin,Users:Users,query:""});
-            }
-
-        });
-
+    else {
+        if (req.query.id != null) {
+            var uid = req.query.id;
+            redis.speakManage(uid, function () {
+                redis.allUser(function (Users) {
+                    res.render('admin/index.html', {admin:req.session.admin, Users:Users});
+                });
+            });
+        } else if (req.query.del != null) {
+            var uid = req.query.del;
+            redis.delUser(uid, function () {
+                redis.allUser(function (Users) {
+                    res.render('admin/index.html', {admin:req.session.admin, Users:Users});
+                });
+            });
+        }
+        else {
+            redis.allUser(function (Users) {
+                res.render('admin/index.html', {admin:req.session.admin, Users:Users});
+            });
+        }
     }
 }
 
